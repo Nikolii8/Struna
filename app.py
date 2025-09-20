2e75a5f300ea5cf7898cca5f507e6eab76bbd09c
Nikolii8 <135508051+Nikolii8@users.noreply.github.com>
2025-09-21 01:03:01 +0300

    s

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 # app.py
 from flask import Flask, jsonify, request, send_from_directory
 import os
-from dotenv import load_dotenv
 from supabase import create_client, Client
-
-load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY") or os.getenv("SUPABASE_KEY")
